9cf172206063e9037b8ad4f18927c6ea8a9e7eeb
chan <chanunlang@gmail.com>
2016-08-24 14:51:51 +0800

    调整路由

--- server/application/config/routes.php
+++ server/application/config/routes.php
@@ -2,8 +2,5 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'home';
-$route['(:any)/list'] = 'pager/mylist/$1';
-$route['(:any)/list/(:num)'] = 'pager/mylist/$1/$2';
-$route['article/(:num)'] = 'pager/article/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
